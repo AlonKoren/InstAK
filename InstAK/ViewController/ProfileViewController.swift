f6068286aa8418349b8941ecafdfd600016bfe98
Alon Koren <alonkoren312@gmail.com>
2020-01-13 20:39:23 +0200

    Design profile view

--- InstAK/ViewController/ProfileViewController.swift
+++ InstAK/ViewController/ProfileViewController.swift
@@ -10,20 +10,28 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        collectionView.dataSource = self
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ProfileViewController:UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerViewCell = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "HeaderProfileCollectionReusableView", for: indexPath) as! HeaderProfileCollectionReusableView
+        headerViewCell.updateView()
+        return headerViewCell
+    }
 }
---
+++ InstAK/Views/HeaderProfileCollectionReusableView.swift
@@ -0,0 +1,41 @@
+//
+//  HeaderProfileCollectionReusableView.swift
+//  InstAK
+//
+//  Created by alon koren on 13/01/2020.
+//  Copyright © 2020 Alon Koren. All rights reserved.
+//
+
+import UIKit
+
+class HeaderProfileCollectionReusableView: UICollectionReusableView {
+    
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var myPostsCountLabel: UILabel!
+    
+    @IBOutlet weak var followingCountLabel: UILabel!
+    
+    @IBOutlet weak var followersCountLabel: UILabel!
+    
+    
+    func updateView() {
+        Api.User.observeUser(withId: AuthService.getCurrentUserId()!, onCompletion: { (user:User) in
+            
+            self.nameLabel.text = user.username
+            if let profileImageUrlString = user.prifileImage {
+                let profileImageUrl = URL(string: profileImageUrlString)
+                let placeholder = #imageLiteral(resourceName: "placeholder-avatar-profile")
+                self.profileImage.kf.setImage(with: profileImageUrl, placeholder: placeholder, options: [])
+            }else{
+                print("profileImageUrlString does not exist")
+            }
+        }) { (err) in
+            
+        }
+    }
+        
+}
