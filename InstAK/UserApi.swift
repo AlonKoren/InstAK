2f0afd13d3fa2edd702c1298d5477be65283e0b6
Alon Koren <alonkoren312@gmail.com>
2020-01-14 14:56:08 +0200

    Discover users to follow
    design a UI

--- InstAK/UserApi.swift
+++ InstAK/UserApi.swift
@@ -34,6 +34,115 @@ class UserApi {
         }
     }
     
+    func observeAllUsers(onAdded: @escaping (User)-> Void , onModified: @escaping (User)-> Void , onRemoved: @escaping (User)-> Void, onError : @escaping (Error)-> Void) ->Listener{
+        
+        let listener:Listener = Listener()
+        listener.firestoreListener = COLLECTION_USERS.addSnapshotListener { (querySnapshot, error) in
+            guard let snapshot = querySnapshot else {
+                onError(error!)
+                return
+            }
+            for diff in snapshot.documentChanges{
+                let user: User = try! DictionaryDecoder().decode(User.self, from: diff.document.data())
+                if (diff.type == .added) {
+                    print("New User : \(user)")
+                    onAdded(user)
+                }
+                if (diff.type == .modified) {
+                    print("Modified User : \(user)")
+                    onModified(user)
+                }
+                if (diff.type == .removed) {
+                    print("Removed User : \(user)")
+                    onRemoved(user)
+                }
+            }
+        }
+        return listener
+    }
+    
+    func observeSpecificUsers(usersIds : [String] ,onAdded: @escaping (User)-> Void , onModified: @escaping (User)-> Void , onRemoved: @escaping (User)-> Void, onError : @escaping (Error)-> Void) ->Listener{
+        
+        let listener:Listener = Listener()
+        listener.firestoreListener = COLLECTION_USERS.whereField("uid", in: usersIds).addSnapshotListener { (querySnapshot, error) in
+            guard let snapshot = querySnapshot else {
+                onError(error!)
+                return
+            }
+            for diff in snapshot.documentChanges{
+                let user: User = try! DictionaryDecoder().decode(User.self, from: diff.document.data())
+                if (diff.type == .added) {
+                    print("New User : \(user)")
+                    onAdded(user)
+                }
+                if (diff.type == .modified) {
+                    print("Modified User : \(user)")
+                    onModified(user)
+                }
+                if (diff.type == .removed) {
+                    print("Removed User : \(user)")
+                    onRemoved(user)
+                }
+            }
+        }
+        return listener
+    }
+    
+    
+    // followerUserId want to follow after followingUserId
+    func followingAfterUser(followerUserId: String, followingUserId: String, onCompletion: @escaping () -> Void, onError : @escaping (Error)-> Void) {
+        let followingsCollection = COLLECTION_USERS.document(followerUserId).collection("followings")
+        followingsCollection.document(followingUserId).setData([followingUserId : true]){
+           (error : Error?) in
+            if let err = error{
+                onError(err)
+                return
+            }
+            
+            onCompletion()
+        }
+    }
+    
+    // followingUserId want to follow by followerUserId
+    func followerAfterUser(followerUserId: String, followingUserId: String, onCompletion: @escaping () -> Void, onError : @escaping (Error)-> Void) {
+        let followersCollection = COLLECTION_USERS.document(followingUserId).collection("followers")
+        followersCollection.document(followerUserId).setData([followerUserId : true]){
+           (error : Error?) in
+            if let err = error{
+                onError(err)
+                return
+            }
+            onCompletion()
+        }
+    }
+    
+    // followerUserId don't want to follow after followingUserId
+    func unfollowingAfterUser(followerUserId: String, followingUserId: String, onCompletion: @escaping () -> Void, onError : @escaping (Error)-> Void) {
+        let followingsCollection = COLLECTION_USERS.document(followerUserId).collection("followings")
+        followingsCollection.document(followingUserId).delete(){
+           (error : Error?) in
+            if let err = error{
+                onError(err)
+                return
+            }
+            
+            onCompletion()
+        }
+    }
+    
+    // followingUserId don't want to follow by followerUserId
+    func unfollowerAfterUser(followerUserId: String, followingUserId: String, onCompletion: @escaping () -> Void, onError : @escaping (Error)-> Void) {
+        let followersCollection = COLLECTION_USERS.document(followingUserId).collection("followers")
+        followersCollection.document(followerUserId).delete(){
+           (error : Error?) in
+            if let err = error{
+                onError(err)
+                return
+            }
+            onCompletion()
+        }
+    }
+    
 
     
 }
--- InstAK/ViewController/ProfileViewController.swift
+++ InstAK/ViewController/ProfileViewController.swift
@@ -22,7 +22,6 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView?.delegate = self
-        
         fechUser()
         
     }
@@ -43,6 +42,7 @@ class ProfileViewController: UIViewController {
         }
         Api.User.observeUser(withId: currentUserId, onCompletion: { (user:User) in
             self.user = user
+            self.title = user.username
             self.collectionView.reloadData()
         }) { (err) in
             print(err)
