a9edb0ca663c2528659e5323face8bbd11de99b4
Alon Koren <alonkoren312@gmail.com>
2020-01-20 14:01:29 +0200

    create a filterVC and connect him to the storyboard

--- InstAK/ViewController/FilterViewController.swift
+++ InstAK/ViewController/FilterViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class FilterViewController: UIViewController {
 
+    @IBOutlet weak var filterPhoto: UIImageView!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func cancelBtn_TouchUpInside(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtn_TouchUpInside(_ sender: Any) {
+    }
+    
+}
 
+extension FilterViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    
 }
