fda1935c637ff326be6516fbdfaf1016683c8fb2
Alon Koren <alonkoren312@gmail.com>
2020-01-20 13:55:44 +0200

    Design a filter storyboard with layouts

---
+++ InstAK/ViewController/FilterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FilterViewController.swift
+//  InstAK
+//
+//  Created by alon koren on 20/01/2020.
+//  Copyright © 2020 Alon Koren. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
